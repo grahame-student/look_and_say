d3c903d5ba5d639b2310eff05dc58c664e15a574
Grahame White <4whitg16@solent.ac.uk>
2022-06-08 11:49:15 +0100

    added placeholder for first test and stub for the SequenceGenerator class

---
+++ SequenceGenerator.py
@@ -0,0 +1,18 @@
+class SequenceGenerator:
+    def __init__(self, start_value):
+        """
+        Constructor for the SequenceGenerator class
+        
+        :param start_value: The initial value to start the sequence from
+        :type  start_value: int
+        """
+        pass
+        
+    def next_value(self):
+        """
+        Generate the next value in the look and see sequence
+        
+        :return: Returns the next value in the look and see sequence
+        :rtype:  string
+        """
+        pass
---
+++ tests/test_sequence_generator.py
@@ -0,0 +1,6 @@
+from unittest import TestCase
+
+
+class TestSequenceGenerator(TestCase):
+    def test_next_value_returns_initial_value_on_first_call(self):
+        pass
