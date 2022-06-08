e7fe39bf3daf288edf6eb9a1e6b97beb13c88175
Grahame White <4whitg16@solent.ac.uk>
2022-06-08 12:50:41 +0100

    fixed import problem

--- tests/test_sequence_generator.py
+++ tests/test_sequence_generator.py
@@ -1,9 +1,9 @@
 from unittest import TestCase
 from hamcrest import assert_that, equal_to
-
+from SequenceGenerator import SequenceGenerator
 
 class TestSequenceGenerator(TestCase):
     def test_next_value_returns_initial_value_on_first_call(self):
         generator = SequenceGenerator(1)
-        
+
         assert_that(generator.next_value(), equal_to(1))
