9916a086d11d3ebf8331342002778879d8d6416b
Grahame White <4whitg16@solent.ac.uk>
2022-06-08 13:08:41 +0100

    address lint issue

--- tests/test_sequence_generator.py
+++ tests/test_sequence_generator.py
@@ -7,6 +7,6 @@ import SequenceGenerator
 
 class TestSequenceGenerator(TestCase):
     def test_next_value_returns_initial_value_on_first_call(self):
-        generator = SequenceGenerator(1)
+        generator = SequenceGenerator.SequenceGenerator(1)
 
         assert_that(generator.next_value(), equal_to(1))
