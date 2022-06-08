e13eb13505d9f1c476d09b2f2bbbebf164ae33be
Grahame White <4whitg16@solent.ac.uk>
2022-06-08 13:19:31 +0100

    fixed unit test

--- tests/test_sequence_generator.py
+++ tests/test_sequence_generator.py
@@ -2,11 +2,11 @@ from unittest import TestCase
 
 from hamcrest import assert_that, equal_to
 
-import SequenceGenerator
+from sequence_generator import SequenceGenerator
 
 
 class TestSequenceGenerator(TestCase):
     def test_next_value_returns_initial_value_on_first_call(self):
-        generator = SequenceGenerator.SequenceGenerator(1)
+        generator = SequenceGenerator(1)
 
-        assert_that(generator.next_value(), equal_to(1))
+        assert_that(generator.next_value(), equal_to("1"))
