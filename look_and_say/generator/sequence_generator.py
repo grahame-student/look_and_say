3bced2acedc3004abd58a171b61953fb680351f0
Grahame White <4whitg16@solent.ac.uk>
2022-06-09 12:34:15 +0100

    added next increment of functionality

--- look_and_say/generator/sequence_generator.py
+++ look_and_say/generator/sequence_generator.py
@@ -6,13 +6,20 @@ class SequenceGenerator:
         :param start_value: The initial value to start the sequence from
         :type  start_value: int
         """
-        pass
+        self.__initial = start_value
+        self.__next = self.__initial
 
     def next_value(self):
         """
-        Generate the next value in the look and see sequence
+        Return the next value in the look and see sequence
 
         :return: Returns the next value in the look and see sequence
         :rtype:  string
         """
-        return f"{1}"
+        result = self.__next
+        self.__next = self.__get_next_value()
+
+        return f"{result}"
+
+    def __get_next_value(self):
+        return "11"
--- look_and_say/tests/test_sequence_generator.py
+++ look_and_say/tests/test_sequence_generator.py
@@ -7,5 +7,9 @@ from look_and_say.generator.sequence_generator import SequenceGenerator
 class TestSequenceGenerator(TestCase):
     def test_next_value_returns_initial_value_on_first_call(self):
         generator = SequenceGenerator(1)
+        assert_that(generator.next_value(), equal_to("1"))
 
+    def test_next_value_returns_11_on_second_call_when_start_value_is_1(self):
+        generator = SequenceGenerator(1)
+        _ = generator.next_value()
         assert_that(generator.next_value(), equal_to("1"))
