6c088fdc6f67e38d46cd731e1067fd6101fd9ff2
Grahame White <4whitg16@solent.ac.uk>
2022-06-09 12:36:34 +0100

    fixed typo in new test case

--- look_and_say/tests/test_sequence_generator.py
+++ look_and_say/tests/test_sequence_generator.py
@@ -12,4 +12,4 @@ class TestSequenceGenerator(TestCase):
     def test_next_value_returns_11_on_second_call_when_start_value_is_1(self):
         generator = SequenceGenerator(1)
         _ = generator.next_value()
-        assert_that(generator.next_value(), equal_to("1"))
+        assert_that(generator.next_value(), equal_to("11"))
